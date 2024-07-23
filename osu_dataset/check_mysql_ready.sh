7a45197eb998a16134f89aa7a96487d5b5edf75c
Eve-ning <johnchangqi@hotmail.com>
2024-07-23 18:29:59 +0800

    Update MySQL Check for dataset

--- osu_dataset/check_mysql_ready.sh
+++ osu_dataset/check_mysql_ready.sh
@@ -5,11 +5,8 @@ attempt=0
 while ((attempt < max_attempts))
 do
   # Check if osu-data is running
-  if docker ps --format '{{.Names}}' | grep -q "^osu.mysql$"; then
-    # Check if MySQL is ready
-    if docker logs osu.mysql 2>&1 | grep -q "ready for connections.*port: 3308"; then
-      exit 0
-    fi
+  if [[ $(docker inspect --format='{{json .State.Health.Status}}' osu.mysql) == "\"healthy\"" ]]; then
+    exit 0
   fi
   echo "Waiting for MySQL to be ready... Attempt $attempt of $max_attempts"
   sleep 10
