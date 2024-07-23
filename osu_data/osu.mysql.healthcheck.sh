8a15b5250c19e223cd9da0ce71cc1cbbf72009ec
Eve-ning <johnchangqi@hotmail.com>
2024-07-23 18:21:39 +0800

    Improve mysql healthchecking

--- osu_data/osu.mysql.healthcheck.sh
+++ osu_data/osu.mysql.healthcheck.sh
@@ -1,8 +1,10 @@
 # Checks the state of the importing.
 # Exit 0 if the import is done, else 1
 
-# If there's no matching processlist, then it's done.
 sleep 10;  # Sleep is necessary to ensure that the initialization has started.
-check="$(mysql -u root -pp@ssw0rd1 -e "SELECT state FROM information_schema.processlist WHERE db='osu'")"
-echo "osu.mysql Health Check: $check"
-[ -z "$check" ] && exit 0 || exit 1
+
+if exec 6<>"/dev/tcp/localhost/$MYSQL_TCP_PORT"; then
+  echo "MySQL is ready!"
+else
+  echo "MySQL is not ready!"
+fi
